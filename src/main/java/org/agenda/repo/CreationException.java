7d323711842dfb5ddede3554267cc553c7201bd8
NSWPOL\ronald.versantvo_nsp <ronald.versantvoort@ns.nl>
2023-07-21 21:09:47 +0200

    Implemented CreationException with throwable, String and Throwable

--- src/main/java/org/agenda/repo/CreationException.java
+++ src/main/java/org/agenda/repo/CreationException.java
@@ -1,7 +1,13 @@
 package org.agenda.repo;
 
-public class CreationException extends RuntimeException{
+public class CreationException extends RuntimeException {
+    public CreationException(String errorMessage, Throwable cause) {
+        super(errorMessage, cause);
+    }
+
     public CreationException(String errorMessage) {
         super(errorMessage);
     }
+
+    public CreationException(Throwable cause) { super(cause); }
 }
